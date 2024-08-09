c7a97ec117c54e9d8716d7115ff3e328a33d3f52
aiswarya-das <aiswaryadas.a@gmail.com>
2024-08-09 19:06:14 +0530

    modified files

--- src/app/components/Visiongoogle.tsx
+++ src/app/components/Visiongoogle.tsx
@@ -1,14 +1,8 @@
 'use client'
 import { useEffect, useState } from 'react';
-<<<<<<< HEAD:src/app/components/Visiongoogle.tsx
 import { getServar } from '../../utils/Vision.getServar';
 import React from "react"
-// import { filePath } from './cam';
 
-=======
-import { getServar } from '../utils/Vision.getServar';
-import React from "react";
->>>>>>> fcf584d (camera component refactor):src/components/Visiongoogle.tsx
 
 interface VisiongoogleProps {
   filePath: string;
